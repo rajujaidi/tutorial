1c1e2a871796f8c0e871679561fc5e0cea5a7bd4
LENOVO-PC\raju <rajreddy4124@gmail.com>
2017-07-25 14:58:26 -0500

    test object created

---
+++ TestOb.java
@@ -0,0 +1,11 @@
+package com.dss;
+
+public class TestOb {
+
+	public static void main(String[] args)throws ClassNotFoundException {
+		Class.forName("sun.odbc.odbcodbcDriver");
+		System.out.println();		
+
+	}
+
+}
