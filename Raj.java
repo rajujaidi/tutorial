f96be00c1c73d9b698feaf5ee6575c35c20bc06a
LENOVO-PC\raju <rajreddy4124@gmail.com>
2017-07-25 14:53:17 -0500

    raj.java added

---
+++ Raj.java
@@ -0,0 +1,17 @@
+//Raj.java
+Import java.lang.String;
+Import java.lang.System;
+
+class  Test
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("raj World!");
+	}
+}
+class A
+{
+}
+class B
+{
+}
